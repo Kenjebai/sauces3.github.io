76eff1ac814319b963f1d40a0d5a9e59b40e5eab
myProjects <142065688+Kenjebai@users.noreply.github.com>
2025-02-04 22:54:21 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,47 +1,93 @@
-button.addEventListener('click', async () => {
-    const perm = await Notification.requestPermission()
-    console.log(perm);
-    
-    if(perm === 'granted'){
-        new Notification('iio', {
-            body: 'lorem lorem lorem',
-            data: {
-                url: '',
-            },
-            icon: 'notify.png',
-            tag: 'notify'
-        })
-    }
-})
-
 function sendwhatsapp(){
-    let phonenumber = "+996702944777";
-    let name = document.querySelector('.name').value;
-    let phone = document.querySelector('.phone').value;
-    let email = document.querySelector('.email').value;
-    let message = document.querySelector('.message').value;
-    let url = "https://wa.me/" + phonenumber + "?text=" 
-    +"*Name :* " +name+"%0a"
-    +"*Phone :* " +phone+"%0a"
-    +"*Email :* " +email+"%0a"
-    +"*Message :* " +message+"%0a%0a";
+  let phonenumber = "+996706831799";
 
-    window.open(url, '_blank').focus();
+  var name1;
+  var data1;
+  function taste(){
+    const qq = document.querySelectorAll('.vals');
+    for (let i = 0; i < qq.length; i++){
+      if(qq[i].checked){
+        data1 = qq[i].value;
+        break;
+      }
+    }
+    name1 = data1;
+  }
+  taste()
+
+  var name2;
+  var data2;
+  function ww(){
+    const ee = document.querySelectorAll('.nasyshennost');
+    for(let i = 0; i < ee.length; i++){
+      if(ee[i].checked){
+        data2 = ee[i].value;
+        break;
+      }
+    }
+    name2 = data2;
+  }
+  ww();
+  
+  var name5;
+  var data5;
+  function japans(){
+    const jj = document.querySelectorAll('.japon');
+    for(let i = 0; i < jj.length; i++){
+      if(jj[i].checked){
+        data5 = jj[i].value;
+        break;
+      }
+    }
+    name5 = data5;
+  }
+  japans();
+
+  var name3;
+  var data3
+  const wa2 = function () {     
+    const inputs = document.querySelectorAll('.grade');
+    console.log(inputs);
+    for (let i = 0; i < inputs.length; i++){
+      if(inputs[i].checked){
+        data3 = inputs[i].value;
+        break;
+      }
+    }
+    name3 = data3;
+  }
+  wa2();
+
+  var name4;
+  var data4;
+  function aa(){
+    const bb = document.querySelectorAll('.cc');
+    for(let i = 0; i < bb.length; i++){
+      if(bb[i].checked){
+        data4 = bb[i].value;
+        break;
+      }
+    }
+    name4 = data4;
+  }
+  aa()
+
+  let name = document.querySelector('.name').value;
+  let phone = document.querySelector('.phone').value;
+  let email = document.querySelector('.email').value;
+  let message1 = document.querySelector('.message2').value;
+  let message = document.querySelector('.message').value;
+  let url = "https://wa.me/" + phonenumber + "?text=" 
+  +"*Как вам вкус соевого соуса? :* " +name1+"%0a"
+  +"*Как вы оцениваете насыщенность вкуса? :* " +name2+"%0a"
+  +"*Чем этот соус отличается от тех, которые вы пробовали раньше? :* " +message1+"%0a"
+  +"*Общая оценка соуса по шкале от 1 до 5 (1 - совсем не понравился, 5 - превосходный соус) :* " +name3+"%0a"
+  +"*Купили бы вы этот соус для домашнего использования? :* " +name4+"%0a"
+  +"*Есть ли что-то, что вы хотели бы улучшить в этом соусе? :* " +message+"%0a"
+  +"*Имя :* " +name+"%0a"
+  +"*Телефон :* " +phone+"%0a"
+  +"*Емайл :* " +email+"%0a%0a";
+    
+  window.open(url, '_blank').focus();
 }
 
-// $(document).ready(function(){
-//     $(".button").click(function(){
-//         $.ajax('https://eu43.chat-api.com/instance**********/message?token=**********', {
-//             data: JSON.stringify({
-//                 phone: $('input[name="recipient"]').val(),
-//                 body: 
-//                 `Имя: ${$('input[name="name"]').val()}\n`+
-//                 `Телефон: ${$('input[name="phone"]').val()}\n`+
-//                 `Емайл: ${$('input[name="email"]').val()}\n`+
-//                 `Заказ: соус "Умами"`
-//             }),
-//             contentType: 'application/json',
-//             type: 'POST'
-//         });
-//     });
-// });
